5577d36a1487a35a4f8fbf113526980cd336e363
M.U <rustamovmuhammadumar2004@gmail.com>
2026-08-25 11:04:05 +0500

    fix: modify updateMember GraphQL Api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -9,6 +9,7 @@ import type { ObjectId } from 'mongoose';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 @Resolver()
 export class MemberResolver {
@@ -25,16 +26,8 @@ export class MemberResolver {
         console.log('Mutation: login');
 		return this.memberService.login(input);
 	}
-// Authenticated
-	@UseGuards(AuthGuard)
-	@Mutation(() => String)
-	public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string> {
-		console.log('Mutation: updateMember');
-		console.log(memberId);
-		return this.memberService.updateMember();
-	}
-
-		@Roles(MemberType.USER, MemberType.AGENT)
+// Authenticated	
+	@Roles(MemberType.USER, MemberType.AGENT)
 	@UseGuards(RolesGuard)
 	@Query(() => String)
 	public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
@@ -50,6 +43,20 @@ export class MemberResolver {
 		console.log('memberNick:', memberNick);
 		return `Hi ${memberNick}`;
 	}
+	
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async updateMember(
+		@Args('input') input: MemberUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('Mutation: updateMember');
+		delete (input as Partial<MemberUpdate>)._id;
+		console.log('memberId:', memberId);
+		return this.memberService.updateMember(memberId, input);
+	}
+
+
 
 	@Query(() => String)
 	public async getMember(): Promise<string> {
@@ -62,7 +69,7 @@ export class MemberResolver {
 
 	// Autoration: ADMIN
 	@Roles(MemberType.ADMIN)
-	@UseGuards(RolesGuard)
+	@UseGuards(RolesGuard)  
 	@Mutation(() => String)
 	public async getAllMembersByAdmin(): Promise<string> {
 		return this.memberService.getAllMembersByAdmin();
