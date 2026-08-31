547e0419858ab71279a0afb42f84cb62582cd5bc
M.U <rustamovmuhammadumar2004@gmail.com>
2026-08-31 17:40:25 +0500

    fix: modification of property model

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { PropertyResolver } from './property.resolver';
+import { PropertyService } from './property.service';
+import { AuthModule } from '../auth/auth.module';
+import PropertySchema from '../../schemas/Property.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ViewModule } from '../view/view.module';
 
-@Module({})
+
+
+@Module({
+	imports: [MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]), AuthModule, ViewModule],
+	providers: [PropertyResolver, PropertyService],
+})
 export class PropertyModule {}
