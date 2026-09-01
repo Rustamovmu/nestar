3b24ffdef87010c29efafd76612a8f0efabbc5b6
M.U <rustamovmuhammadumar2004@gmail.com>
2026-09-01 15:55:26 +0500

    feat: develop removeCommentByAdmin GraphQl api

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -50,4 +50,14 @@ export class CommentResolver {
 		const result = await this.commentService.getComments(memberId, input);
 		return result;
 	}
+
+    	//**ADMIN */
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Comment)
+	public async removeCommentByAdmin(@Args('commentId') input: string): Promise<Comment> {
+		console.log('Mutation: removeCommentByAdmin');
+		const commentId = shapeIntoMongoObjectId(input);
+		return await this.commentService.removeCommentByAdmin(commentId);
+	}
 }
