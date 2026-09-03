d3cc203435ce3d0c352539fa9c4cd87134f48707
M.U <rustamovmuhammadumar2004@gmail.com>
2026-09-03 14:36:06 +0500

    feat: developed subscribe, unsubscribe, getMemberFollowings and getMemberFollowers graphql api

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -20,6 +20,7 @@ export const availableCommentSorts = ['createdAt', 'updatedAt'];
 /** IMAGE CONFIGURATION **/
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
+import { from } from 'rxjs';
 
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = (filename: string) => {
@@ -39,3 +40,21 @@ export const lookupMember = {
 		as: 'memberData',
 	},
 };
+
+export const lookupFollowingData = {
+    $lookup: {
+        from: "members",
+        localField: "followingId",
+        foreignField: "_id",
+        as: "followingData"
+    }
+}
+
+export const lookupFollowerData = {
+    $lookup: {
+        from: "members",
+        localField: "followerId",
+        foreignField: "_id",
+        as: "followerData"
+    }
+}
