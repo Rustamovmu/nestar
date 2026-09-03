5459bccb4803db73db790de57b6ac02eb687831c
M.U <rustamovmuhammadumar2004@gmail.com>
2026-09-03 13:45:36 +0500

    feat: developed liketargetProperty graphql api

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -6,6 +6,7 @@ import PropertySchema from '../../schemas/Property.model';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ViewModule } from '../view/view.module';
 import { MemberModule } from '../member/member.module';
+import { LikeModule } from '../like/like.module';
 
 
 
@@ -20,6 +21,7 @@ imports: [
 		AuthModule,
 		ViewModule,
 		MemberModule,
+		LikeModule
 	],
 	providers: [PropertyResolver, PropertyService],
 	exports: [PropertyService],
